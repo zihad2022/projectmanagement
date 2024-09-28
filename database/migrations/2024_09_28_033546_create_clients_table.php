9797b66762ed0bacb12b542e888e47e1989daa4d
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-28 15:45:05 +0600

    change address type from string to text

--- database/migrations/2024_09_28_033546_create_clients_table.php
+++ database/migrations/2024_09_28_033546_create_clients_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
-            $table->string('address');
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
