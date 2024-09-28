d2ca923505ed707314f5d2a676658bed6458403c
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-28 19:10:06 +0600

    change status type to integer

--- database/migrations/2024_09_28_033706_create_milestones_table.php
+++ database/migrations/2024_09_28_033706_create_milestones_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->date('deadline');
-            $table->enum('status', ['Pending', 'Completed'])->default('Pending');
+            $table->integer('status');
             $table->timestamps();
         });
     }
