2482050b62172a5663d259aaa7dcc0d5b6feeb08
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-28 14:54:02 +0600

    create milestone factory

---
+++ database/factories/MilestoneFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Milestone>
+ */
+class MilestoneFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(2),
+            'deadline' => Carbon::now()->addDays($this->faker->numberBetween(5, 30)),
+            'project_id' => \App\Models\Project::factory(),
+        ];
+    }
+}
