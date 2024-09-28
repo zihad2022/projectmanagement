0386ea24a8d4b051d3a167068a71bcc12d11bff5
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-28 14:55:10 +0600

    create team member  factory

---
+++ database/factories/TeamMemberFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TeamMember>
+ */
+class TeamMemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'salary' => $this->faker->numberBetween(20000, 80000),
+            'success_rate' => $this->faker->numberBetween(50, 100),
+        ];
+    }
+}
