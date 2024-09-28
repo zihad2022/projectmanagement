622189db4cd4e55f3db6e85e7b80fdca6b98132c
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-28 14:54:23 +0600

    create payment method factory

---
+++ database/factories/PaymentMethodFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PaymentMethod>
+ */
+class PaymentMethodFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(['Bank', 'Binance', 'Payoneer']),
+            'currency' => $this->faker->randomElement(['BDT', 'USD', 'EUR', 'USDT']),
+        ];
+    }
+}
