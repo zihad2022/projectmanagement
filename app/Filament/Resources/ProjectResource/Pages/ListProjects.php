d4b900db9cddca04069e3eee981e4bdb28b1ce90
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-29 10:46:45 +0600

    add tabs for easily filter with status

--- app/Filament/Resources/ProjectResource/Pages/ListProjects.php
+++ app/Filament/Resources/ProjectResource/Pages/ListProjects.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\ProjectResource\Pages;
 
+use App\Enums\ProjectStatus;
 use App\Filament\Resources\ProjectResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProjects extends ListRecords
@@ -14,6 +16,22 @@ class ListProjects extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All Projects'),
+            'pending' => Tab::make('Pending')->modifyQueryUsing(function ($query) {
+                return $query->where('status', ProjectStatus::Pending);
+            }),
+            'in_progress' => Tab::make('In Progress')->modifyQueryUsing(function ($query) {
+                return $query->where('status', ProjectStatus::InProgress);
+            }),
+            'completed' => Tab::make('Completed')->modifyQueryUsing(function ($query) {
+                return $query->where('status', ProjectStatus::Completed);
+            }),
         ];
     }
 }
