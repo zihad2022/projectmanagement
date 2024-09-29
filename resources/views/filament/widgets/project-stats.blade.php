e095107d8283c6547e945d7112155d63925eadf3
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-29 13:55:05 +0600

    logic modify for display every project in dashboard seperately

--- resources/views/filament/widgets/project-stats.blade.php
+++ resources/views/filament/widgets/project-stats.blade.php
@@ -1,11 +1,9 @@
 <div class="space-y-4">
     <ul>
-        @foreach ($projectsWithDaysLeft as $project)
-            <li class="p-4 bg-gray-600 shadow rounded">
-                <strong>{{ $project['name'] }}</strong><br>
-                <span>Days left: {{ $project['days_left'] }}</span><br>
-                <span>Progress: {{ $project['progress'] }}%</span>
-            </li>
-        @endforeach
+        <li class="p-4 bg-gray-600 shadow rounded">
+            <strong>{{ $project['name'] }}</strong><br>
+            <span>Days left: {{ $project['days_left'] }}</span><br>
+            <span>Progress: {{ $project['progress'] }}%</span>
+        </li>
     </ul>
 </div>
