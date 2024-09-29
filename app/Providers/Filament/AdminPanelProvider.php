f0ecfd6bec4949146dd1b142c87fb6107ff5cdab
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-29 10:40:12 +0600

    add max content width is full

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -28,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->spa()
+            ->maxContentWidth(MaxWidth::Full)
             ->colors([
                 'primary' => Color::Blue,
             ])
