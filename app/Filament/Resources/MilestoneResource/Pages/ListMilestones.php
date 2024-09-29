54f6389c3ef927eb319f6ea011a2568637a227b5
zihad2022 <zihadulislamafnan@gmail.com>
2024-09-29 11:12:05 +0600

    add tabs for easily filter with status

--- app/Filament/Resources/MilestoneResource/Pages/ListMilestones.php
+++ app/Filament/Resources/MilestoneResource/Pages/ListMilestones.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\MilestoneResource\Pages;
 
+use App\Enums\MilestoneStatus;
 use App\Filament\Resources\MilestoneResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListMilestones extends ListRecords
@@ -14,6 +16,19 @@ class ListMilestones extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'pending' => Tab::make('Pending')->modifyQueryUsing(function ($query) {
+                return $query->where('status', MilestoneStatus::Pending);
+            }),
+            'completed' => Tab::make('Completed')->modifyQueryUsing(function ($query) {
+                return $query->where('status', MilestoneStatus::Completed);
+            }),
         ];
     }
 }
--- app/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\TaskResource\Pages;
 
+use App\Enums\TaskStatus;
 use App\Filament\Resources\TaskResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTasks extends ListRecords
@@ -14,6 +16,22 @@ class ListTasks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'pending' => Tab::make('Pending')->modifyQueryUsing(function ($query) {
+                return $query->where('status', TaskStatus::Pending);
+            }),
+            'in_progress' => Tab::make('In Progress')->modifyQueryUsing(function ($query) {
+                return $query->where('status', TaskStatus::InProgress);
+            }),
+            'completed' => Tab::make('Completed')->modifyQueryUsing(function ($query) {
+                return $query->where('status', TaskStatus::Completed);
+            }),
         ];
     }
 }
